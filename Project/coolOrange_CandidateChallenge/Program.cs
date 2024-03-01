f64079bc1fb0eae69b2baa2d0e434ee10934502c
Domain.egg <alex.domanegg@outlook.com>
2024-03-01 11:31:34 +0100

    Project setup
    Program runs
    Tests run and fail

--- Project/coolOrange_CandidateChallenge/Program.cs
+++ Project/coolOrange_CandidateChallenge/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace coolOrange_CandidateChallenge
+﻿using System;
+
+namespace coolOrange_CandidateChallenge
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
         }
     }
 }
