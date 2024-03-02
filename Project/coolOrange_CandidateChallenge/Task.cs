5565aa01742f657bb8401f9bc5f6126acd20382d
Domain.egg <alex.domanegg@outlook.com>
2024-03-02 10:07:50 +0100

    Task
    Task.cs created
    has Field name, can be set with CTOR

---
+++ Project/coolOrange_CandidateChallenge/Task.cs
@@ -0,0 +1,12 @@
+namespace coolOrange_CandidateChallenge
+{
+    public class Task
+    {
+        private string name;
+
+        public Task(string name)
+        {
+            this.name = name;
+        }
+    }
+}
