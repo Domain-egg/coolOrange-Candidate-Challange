63fb1638958929fe5d866d3c158a9dda3737cb18
Domain.egg <46201140+Domain-egg@users.noreply.github.com>
2024-03-02 09:16:35 +0100

    Array (#1)
    * FindMaxValue
    
    implemented and tested
    works as expected
    
    * FindMinPosition
    
    implemented and tested
    works as expected
    
    * Swap done
    
    implemented and tested
    works as expected
    
    * ShiftLeftByOne
    
    implemented and tested
    works as expected
    
    * CreateRandomArray
    
    implemented and tested
    works as expected
    
    did some Code reformatting
    
    * CreateRandomMatrix
    
    implemented and tested
    
    NOTE: Test or specs needed change, .GetLength is not supported on jagged Arrays.
    CreateRandomMatrix return type changed to Multidimensional Array
    
    * CopyArray
    
    implemented and tested
    works as expected
    
    NOTE: change of specs CopyArray returns 2D-Array instead of jagged-Array
    
    * FindInSortedArray
    
    implemented and tested
    works as expected
    
    used build in BinarySearch
    
    * Array finished
    
    reviewed Code and run all tests related to Array.cs

--- Project/coolOrange_CandidateChallenge/Array.cs
+++ Project/coolOrange_CandidateChallenge/Array.cs
@@ -1,50 +1,94 @@
 ﻿using System;
+using System.Linq;
 
 namespace coolOrange_CandidateChallenge
 {
-	public class Array
-	{
-		//ToDo
+    public class Array
+    {
+        public static int FindMaxValue(int[] array, int position1, int position2)
+        {
+            var length = position2 - position1 + 1;
+            var slicedArray = new int[length];
+            System.Array.Copy(array, position1, slicedArray, 0, length);
+            return slicedArray.Max();
+        }
 
-		public static int FindMaxValue(int[] array, int position1, int position2)
-		{
-			throw new NotImplementedException();
-		}
+        public static int FindMinPosition(int[] array, int position1, int position2)
+        {
+            var length = position2 - position1 + 1;
+            var slicedArray = new int[length];
+            System.Array.Copy(array, position1, slicedArray, 0, length);
+            return System.Array.IndexOf(array, slicedArray.Min());
+        }
 
-		public static int FindMinPosition(int[] array, int position1, int position2)
-		{
-			throw new NotImplementedException();
-		}
+        public static void Swap(int[] array, int position1, int position2)
+        {
+            var valueOfPosition2 = array.GetValue(position2);
+            array.SetValue(array.GetValue(position1), position2);
+            array.SetValue(valueOfPosition2, position1);
+        }
 
-		public static void Swap(int[] array, int position1, int position2)
-		{
-			throw new NotImplementedException();
-		}
+        public static void ShiftLeftByOne(int[] array, int position1, int position2)
+        {
+            var firstValue = array.GetValue(position1);
+            for (var i = position1; i < position2 - 1; i++)
+            {
+                array.SetValue(array.GetValue(i + 1), i);
+            }
 
-		public static void ShiftLeftByOne(int[] array, int position1, int position2)
-		{
-			throw new NotImplementedException();
-		}
+            array.SetValue(firstValue, position2 - 1);
+        }
 
-		public static int[] CreateRandomArray(int size, int minValue, int maxValue)
-		{
-			throw new NotImplementedException();
-		}
+        public static int[] CreateRandomArray(int size, int minValue, int maxValue)
+        {
+            var array = new int[size];
 
-		public static int[][] CreateRandomMatrix(int rows, int columns, int minValue, int maxValue)
-		{
-			throw new NotImplementedException();
-		}
+            var random = new Random();
+            for (var i = 0; i < array.Length; i++)
+            {
+                array.SetValue(random.Next(minValue, maxValue), i);
+            }
 
-		public static int[][] CopyArray(int[] array)
-		{
-			throw new NotImplementedException();
-		}
+            return array;
+        }
 
-		public static int FindInSortedArray(int[] array, int number)
-		{
-			throw new NotImplementedException();
-		}
+        //changed jagged array to two-dimensional array, since in test .GetLength(<dimension>) was used but does not work with jagged arrays
+        public static int[,] CreateRandomMatrix(int rows, int columns, int minValue, int maxValue)
+        {
+            int[,] array2D = new int[rows,columns];
+            for (var i = 0; i < array2D.GetLength(0); i++)
+            {
+                var row = CreateRandomArray(columns, minValue, maxValue);
+                for (var j = 0; j < row.Length; j++)
+                {
+                    array2D[i, j] = row[j];
+                }
+                
+            }
 
-	}
+            return array2D;
+        }
+
+        //changed jagged array to two-dimensional array, since in test .GetLength(<dimension>) was used but does not work with jagged arrays
+        public static int[,] CopyArray(int[] array)
+        {
+            var result = new int[2,array.Length];
+
+            var copy = new int[array.Length];
+            array.CopyTo(copy, 0);
+            
+            for (var i = 0; i < array.Length; i++)
+            {
+                result[0, i] = array[i];
+                result[1, i] = copy[i];
+            }
+            
+            return result;
+        }
+
+        public static int FindInSortedArray(int[] array, int number)
+        {
+            return System.Array.BinarySearch(array, 0, array.Length, number);
+        }
+    }
 }
